17f37f3b08bbf5c94999f739ce58338aa9bf42a9
zhongxian <zhzhxian@163.com>
2026-01-11 21:22:41 +0800

    运行时参数设置

--- spring-ai-quickstart/src/main/java/com/example/springaiquickstart/controller/ChatController.java
+++ spring-ai-quickstart/src/main/java/com/example/springaiquickstart/controller/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,5 +55,29 @@ public class ChatController {
         Flux<String> resp = stream.mapNotNull(chatResponse -> chatResponse.getResult().getOutput().getText());
         System.out.println("返回的内容：" + resp);
         return resp;
+    }
+
+    @GetMapping("/runtimeOptions")
+    public String runtimeOptions(@RequestParam("message") String message
+            , @RequestParam(value = "temp", required = false) Double temp) {
+        System.out.println("收到消息，message = " + message);
+
+        Prompt prompt;
+        if (temp != null) {
+            //构建代temperature的DeepSeekChatOptions，覆盖默认的temperature
+            DeepSeekChatOptions options = DeepSeekChatOptions.builder()
+                    .temperature(temp)
+                    .build();
+            prompt = new Prompt(message, options);
+            System.out.println("运行时覆盖temperature，temp= " + temp);
+        } else {
+            //无temperature传入的时，使用默认配置
+            prompt = new Prompt(message);
+            System.out.println("使用默认的temperature");
+        }
+        ChatResponse chatResponse = chatModel.call(prompt);
+        String text = chatResponse.getResult().getOutput().getText();
+        System.out.println("模型返回结果:" + text);
+        return text;
     }
 }
